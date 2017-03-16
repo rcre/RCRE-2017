dcee20cc324b479cd31b7d98d52585f463ae7eb4
Patrick Hanus <patrick@skymse.com>
2017-03-15 20:39:25 -0400

    Nothing to see here

--- page.php
+++ page.php
@@ -13,11 +13,11 @@
 		</header>
 		<?php } else { ?> <?php	} ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+	<div id="inner-content" class="wrap cf">
 
-								<header class="article-header">
+		<main id="main" class="m-all t-all d-10of12 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
