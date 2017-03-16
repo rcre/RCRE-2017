52c8716a7ad376c6fe813c0eed0b4a927b6c9c68
Patrick Hanus <patrick@skymse.com>
2017-03-15 20:00:56 -0400

    Added a mobile menu and menu location
    Edited the bones.php file.

--- library/bones.php
+++ library/bones.php
@@ -210,7 +210,8 @@ function bones_theme_support() {
 	register_nav_menus(
 		array(
 			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
-			'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
+			'footer-links' => __( 'Footer Links', 'bonestheme' ), // secondary nav in footer
+			'mobile-nav' => __( 'The Mobile Menu', 'bonestheme' ) // secondary nav in footer
 		)
 	);
 
