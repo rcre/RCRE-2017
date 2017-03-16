fc928bc5723b932c7b954fefde7a48b03c02c7fa
Patrick Hanus <patrick@skymse.com>
2017-03-15 20:38:02 -0400

    Added Callout Section
    Should be a standard for pages

--- page.php
+++ page.php
@@ -4,14 +4,14 @@
 		<?php if (has_post_thumbnail($post->ID)) { ?>
 		<?php $image = wp_get_attachment_url(get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail'); ?>
 		<header class="article-header" style="background-image: url('<?php echo $image; ?>');">
+			<div class="callout m-all t-1of6 d-1of3">
+				<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<a href="#" id="cta-white" class="btn">Search</a>
+				<a href="#" id="cta-border" class="btn">Find People</a>
+			</div>
+		</header>
+		<?php } else { ?> <?php	} ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
