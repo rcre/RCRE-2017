e14d42eaafdf775aded0c24d4cd07e5d6a9e5c29
Patrick Hanus <patrick@skymse.com>
2017-03-21 09:23:21 -0400

    PHP Imported new Google Fonts

--- functions.php
+++ functions.php
@@ -230,16 +230,9 @@ function bones_comments( $comment, $args, $depth ) {
 <?php
 } // don't remove this bracket!
 
-
-/*
-This is a modification of a function found in the
-twentythirteen theme where we can declare some
-external fonts. If you're using Google Fonts, you
-can replace these fonts, change it in your scss files
-and be up and running in seconds.
-*/
+// Import Google Fonts
 function bones_fonts() {
-  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+  wp_enqueue_style('googleFonts', 'https://fonts.googleapis.com/css?family=Merriweather:300,400|Raleway:300,400,700');
 }
 
 add_action('wp_enqueue_scripts', 'bones_fonts');
