c1c3a928e822168ffdfa88fcbe8d049d779df3d7
Patrick Hanus <patrick@skymse.com>
2017-03-24 15:18:48 -0400

    Created a service Grid Template

---
+++ library/partials/serviceGrid.php
@@ -0,0 +1,60 @@
+<?php 
+// Template: Service Grid
+?>
+
+
+<section class="service-grid cf">
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-sale.svg" alt="">
+			<h4><a href="#">Buy</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+	
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box m-end"">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-lease.svg" alt="">
+			<h4><a href="#">Lease</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+	
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box d-end">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-sale.svg" alt="">
+			<h4><a href="#">Sell</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+	
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box m-end">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-cash.svg" alt="">
+			<h4><a href="#">Invest</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+	
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-advice.svg" alt="">
+			<h4><a href="#">Get Advice</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+	
+	<div class="m-1of2 t-1of3 d-1of3">
+		<div class="service-box m-end d-end">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-research.svg" alt="">
+			<h4><a href="#">Learn</a></h4>
+			<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
+			<a href="#">Quick Link</a>
+		</div>
+	</div>
+</section>
