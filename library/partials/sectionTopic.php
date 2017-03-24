421bbbc890af0da5be30b0a95b4127be261409e4
Patrick Hanus <patrick@skymse.com>
2017-03-24 15:18:34 -0400

    HTML Created a topic section template

---
+++ library/partials/sectionTopic.php
@@ -0,0 +1,14 @@
+<!-- This will have the background image -->
+	<section class="topic cf">
+	<!-- This will have the SVG slant -->
+		
+	<!-- This part holds the main content in place in the section -->
+		<aside class="m-all t-1of3 d-1of3">
+			<h3>Service</h3>
+			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
+			</p>
+			<a id="cta-border-gray" href="#">View Our Services</a>
+		</aside>
+
+		<div id="services" class="slant right"></div>
+	</section>
