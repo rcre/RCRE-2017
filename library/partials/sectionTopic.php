e60ce537f669ad5772636e7da8551e49943cd3e8
Patrick Hanus <patrick@skymse.com>
2017-03-31 17:03:34 -0400

    Deleted Comments

--- library/partials/sectionTopic.php
+++ library/partials/sectionTopic.php
@@ -12,19 +12,13 @@
 
 		<div class="slant"></div>
 	</section>
-
-
-
+	
 	<section class="topic cf">
-	<!-- This will have the SVG slant -->
-		
-	<!-- This part holds the main content in place in the section -->
 		<aside class="m-all t-1of3 d-1of3 left">
 			<h3>"Service"</h3>
 			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
 			</p>
 			<a id="cta-border-green" href="#">View Our Services</a>
 		</aside>
-
 		<div class="slant-right"></div>
 	</section>
