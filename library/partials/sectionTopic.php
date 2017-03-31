9681814974127b8377896da1bb9dd6b127ebecdb
Patrick Hanus <patrick@skymse.com>
2017-03-31 15:28:43 -0400

    Added a new "right/left" class
    this is for the aside, just telling it which side to float to.

--- library/partials/sectionTopic.php
+++ library/partials/sectionTopic.php
@@ -3,7 +3,7 @@
 	<!-- This will have the SVG slant -->
 		
 	<!-- This part holds the main content in place in the section -->
-		<aside class="m-all t-1of3 d-1of3">
+		<aside class="m-all t-1of3 d-1of3 right">
 			<h3>"Service"</h3>
 			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
 			</p>
