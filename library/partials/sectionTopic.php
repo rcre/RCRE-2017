2d058d666d3334e9e77cffb20ee9884af191f7a5
Patrick Hanus <patrick@skymse.com>
2017-04-05 11:03:14 -0400

    Updated homepage copywriting

--- library/partials/sectionTopic.php
+++ library/partials/sectionTopic.php
@@ -4,7 +4,7 @@
 		
 	<!-- This part holds the main content in place in the section -->
 		<aside class="m-all t-1of3 d-1of3 right">
-			<h3>"Service"</h3>
+			<h3>Services</h3>
 			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
 			</p>
 			<a id="cta-border-green" href="#">View Our Services</a>
@@ -15,10 +15,10 @@
 	
 	<section class="topic cf">
 		<aside class="m-all t-1of3 d-1of3 left">
-			<h3>"Service"</h3>
+			<h3>Specialties and Industries</h3>
 			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
 			</p>
-			<a id="cta-border-green" href="#">View Our Services</a>
+			<a id="cta-border-green" href="#">View Our Specialties</a>
 		</aside>
 		<div class="slant-right"></div>
 	</section>
