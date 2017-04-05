43bd38611f987dd0e8a1477f54ab488e7e2b6427
Patrick Hanus <patrick@skymse.com>
2017-04-05 11:03:29 -0400

    Added the UVP section on the homepage

--- front-page.php
+++ front-page.php
@@ -38,7 +38,28 @@
 
 			<!-- SECTION 1: Service Grid -->
 				<?php get_template_part('library/partials/serviceGrid'); ?>
-			<!-- SECTION 2: See our Services -->
+			
+			<!-- SECTION 2: UVP "Unique Value Proposition" -->
+			<section class="cf">
+				<div class="uvp m-all t-all d-2of3">
+					<p>From landlords and tenants to buyers and sellers, we provide consultative commercial real estate solutions focused on building <strong>long-term relationships</strong> with you and <strong>long-term success</strong>for your business.</p>
+				</div>
+				<div class="selling-point m-all t-1of3 d-1of3">
+					<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icons-book-knowledge.svg" alt="Real Estate Knowledge">
+					<h5>A wealth of knowledge.</h5>
+					<p>With over 600 years of combined experience, our team of experts provides you with the latest relevant data, marketing strategies and superior support you deserve.</p>
+				</div>
+				<div class="selling-point m-all t-1of3 d-1of3">
+					<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icons-book-knowledge.svg" alt="Real Estate Knowledge">
+					<h5>Locally-owned.</h5>
+					<p>As the largest, locally-owned firm in Indianapolis, our brokers are deeply-connected experts who care about the success of the city we live in. We are proud of our roots!</p>
+				</div>
+				<div class="selling-point m-all t-1of3 d-1of3">
+					<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icons-book-knowledge.svg" alt="Real Estate Knowledge">
+					<h5>Free to focus on you.</h5>
+					<p>We don’t answer to stockholders. We aren’t an oversized corporation that only cares about boosting their numbers. We care about your numbers.</p>
+				</div>
+			</section>
 			<!-- SECTION 2: See our Services -->
 				<?php get_template_part('library/partials/sectionTopic'); ?>
 			<!-- SECTION 3: See our Specialties and Industries -->
