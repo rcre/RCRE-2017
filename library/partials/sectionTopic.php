4d93d12a85cf34288c28b36e6f07797a357d2afa
Patrick Hanus <patrick@skymse.com>
2017-04-10 08:57:59 -0400

    Added imports to the style.scss

--- library/partials/sectionTopic.php
+++ library/partials/sectionTopic.php
@@ -10,16 +10,27 @@
 			<a id="cta-border-green" href="#">View Our Services</a>
 		</aside>
 
-		<div class="slant--left"></div>
+		<div class="slant slant--left"></div>
 
 	</section>
 	
 	<section class="topic cf">
 		<aside class="m-all t-1of4 d-1of4 pull-r-1of12 left">
 			<h3>Specialties and Industries</h3>
-			<p>Your business could be eligible for economic incentives you didn't know existed. This is money local and state governments use to attract growing businesses to their area. At RESOURCE, we help you find every bit of savings you could possibly get.
+			<p>Get the funding you need for acquisition, development or refinancing. RESOURCE provides debt and equity solutions for any type of real estate project.
 			</p>
 			<a id="cta-border-green" href="#">View Our Specialties</a>
+			<a id="cta-border-gray" href="#">Meet Our Brokers</a>
 		</aside>
-		<div class="slant--right"></div>
+		<div class="slant slant--right"></div>
+	</section>
+
+	<section class="topic cf">
+		<aside class="m-all t-1of4 d-1of4 pull-l-1of12 right">
+			<h3>Case Studies</h3>
+			<p>A no-hoops approach gets you a long way. When you do good work, it speaks for itself. Here are just a few of those real stories that we want to share with you.
+			</p>
+			<a id="cta-border-green" href="#">Read the Case Studies</a>
+		</aside>
+		<div class="slant slant--left"></div>
 	</section>
