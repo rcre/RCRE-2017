c29f4a260a8710d4212d8010cc4eda5ef57a158b
Patrick Hanus <patrick@skymse.com>
2017-04-10 08:52:16 -0400

    Changed link styles for the service grid

--- library/partials/serviceGrid.php
+++ library/partials/serviceGrid.php
@@ -7,54 +7,54 @@
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-sale.svg" alt="">
-				<h4><a href="#">Buy a Property</a></h4>
+				<h4><a class="header-link" href="#">Buy a Property</a></h4>
 				<p>Check out our listings, or view our specialties</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 		
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box m-end"">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-lease.svg" alt="">
-				<h4><a href="#">Lease a Property</a></h4>
+				<h4><a class="header-link" href="#">Lease a Property</a></h4>
 				<p>Check out our listings, view our specialties.</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 		
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box d-end">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-sale.svg" alt="">
-				<h4><a href="#">Sell</a></h4>
+				<h4><a class="header-link" href="#">Sell</a></h4>
 				<p>Let us help you find the perfect buyer for your property.</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 		
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box m-end">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-cash.svg" alt="">
-				<h4><a href="#">Invest</a></h4>
+				<h4><a class="header-link" href="#">Invest</a></h4>
 				<p>Ranging from student housing to senior living, expand and secure your investment portfolio. Worry free.</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 		
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icons-speech-consultation.svg" alt="">
-				<h4><a href="#">Get Advice</a></h4>
+				<h4><a class="header-link" href="#">Get Advice</a></h4>
 				<p>Don’t burden yourself! Let an expert help you get you what you need. No strings attached.</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 		
 		<div class="m-1of2 t-1of3 d-1of3">
 			<div class="service-box m-end d-end">
 				<img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon-computer.svg" alt="">
-				<h4><a href="#">Learn</a></h4>
+				<h4><a class="header-link" href="#">Learn</a></h4>
 				<p>What good is knowledge if you don’t share? We compiled a database of info, free for you.</p>
-				<a href="#">Quick Link</a>
+				<a class="blue-link" href="#">Quick Link</a>
 			</div>
 		</div>
 	</div>
