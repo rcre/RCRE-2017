77222930a60ca7b09c199a29efd0b1e0e5d402da
Patrick Hanus <patrick@skymse.com>
2017-06-12 17:55:53 -0400

    Changed mini profile for brokers

--- post-formats/content-miniProfile.php
+++ post-formats/content-miniProfile.php
@@ -1,6 +1,9 @@
 <?php // This is the template for a broker's picture, name, title and link to profile only ?>
 	<?php // the div ID is the broker's name with dashes were needed ?>
-	<div id="<?php echo sanitize_title_with_dashes( $title ); ?>" class="employee d-1of4 t-1of3 m-1of2">
+	
+
+
+	<div id="<?php echo sanitize_title_with_dashes( $title ); ?>" class="employee">
 		
 		<?php // Get the link to broker's picture and get alt text ?>
 		<img class="broker-profile-picture" src="<?php echo types_render_field( "broker-profile-picture", array( 'raw' => true)); ?>" alt="<?php echo the_title(); ?>">
@@ -14,3 +17,4 @@
 		<?php // get the link to the broker's profile page ?>
 		<a id="cta-underline-gray" href="<?php echo get_post_permalink(); ?>">View Profile</a>
 	</div>
+
