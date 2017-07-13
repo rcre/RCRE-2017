1193f265b4c4448012c6e8260b9a70938f2bf622
Patrick Hanus <patrick@skymse.com>
2017-07-13 14:57:35 -0400

    Added New Specialties Section
    For general use across the site

---
+++ library/partials/sectionSpecialties.php
@@ -0,0 +1,9 @@
+<?php // SECTION Specialties ?>
+
+<div id="Specialties" class="pull-r-1of12 pull-l-1of12 cf">
+	<div class="specialty--Office d-1of5 t-1of2 m-all">Office</div>
+	<div class="specialty--Retail d-1of5 t-1of2 m-all">Retail</div>
+	<div class="specialty--Industrial d-1of5 t-1of2 m-all">Industrial</div>
+	<div class="specialty--Multifamily d-1of5 t-1of2 m-all">Multifamily</div>
+	<div class="specialty--Land d-1of5 t-all m-all">Land</div>
+</div>
