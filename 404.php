98f7c9f168177e82f0a4aa281c0751cac43d953c
Patrick Hanus <patrick@skymse.com>
2017-07-31 17:26:58 -0400

    404 page edits
    -Cleaned up the 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,3 @@
-<?php 
-
-/*
- Template Name: Error 404
-*/
-?>
-
 <?php get_header(); ?>
 
 <div id="content">
@@ -14,12 +7,6 @@
 		<main id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 			<article id="post-not-found" class="hentry cf">
-
-				<section class="entry-content">
-
-					<p><?php _e( 'Whoops! We cannot seem to find the page you are looking for. Let us know you could not find it and we will get right on finding a solution.', 'bonestheme' ); ?></p>
-
-				</section>
 
 				<section class="topic search">
 
