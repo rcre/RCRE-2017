6b71785237a1448b8d6210a73ea1b55b89ddfa0b
Patrick Hanus <patrick@skymse.com>
2018-01-16 09:22:10 -0500

    Formatting

--- header.php
+++ header.php
@@ -110,9 +110,13 @@
 
 	<body <?php body_class(); ?>">
 		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSZKKGS"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSZKKGS" height="0" width="0" style="display:none;visibility:hidden">
+			</iframe>
+		</noscript>
 		<!-- End Google Tag Manager (noscript) -->
+
+		
 	 <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
